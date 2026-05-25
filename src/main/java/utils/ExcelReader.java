304bac7c70785f4d948a5d17ef7f9579ef77b52c
YousefFawzy2222 <117592164+YousefFawzy2222@users.noreply.github.com>
2026-05-25 13:04:34 +0300

    Added simulation on how to use excelReaders

--- src/main/java/utils/ExcelReader.java
+++ src/main/java/utils/ExcelReader.java
@@ -1,4 +1,29 @@
 package utils;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+
 public class ExcelReader {
+    //bad practice
+    //row and col > start from 0
+    private static final String TEST_DATA_PATH = "src/test/resource/test-data/";
+    public static String getExcelData(String filePath, String sheetName, int rowNum, int cellNum){
+        XSSFWorkbook workbook;
+        XSSFSheet sheet;
+
+        String cellData;
+        try{
+            String excelFileName = "";
+            workbook = new XSSFWorkbook(TEST_DATA_PATH + excelFileName);
+            sheet = workbook.getSheet(sheetName);
+            cellData = sheet.getRow(rowNum-1).getCell(cellNum-1).getStringCellValue();
+            workbook.close();
+            return cellData;
+        }catch (IOException e){
+            System.out.println("Error reading Excel file: " + e.getMessage());
+            return "";
+        }
+    }
 }
--- src/main/java/utils/JsonReader.java
+++ src/main/java/utils/JsonReader.java
@@ -22,11 +22,11 @@ import java.io.FileReader;
 public class JsonReader {
     String jsonReader;
     String jsonFileName;
-    private final String test_data_path = "src/test/java/test-data";
+    private final String test_data_path = "src/test/java/test-data/";
     public JsonReader(String jsonFileName){
         this.jsonFileName = jsonFileName;
         try{
-            JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(test_data_path +"/"+ jsonFileName+".json"));
+            JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(test_data_path + jsonFileName+".json"));
             jsonReader = data.toJSONString();
         } catch (Exception e) {
             System.out.println("Error reading JSON file: " + e.getMessage());
