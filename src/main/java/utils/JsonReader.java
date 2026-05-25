0cc829da592a95cf2aa9b951c4ace2d236345175
YousefFawzy2222 <117592164+YousefFawzy2222@users.noreply.github.com>
2026-05-25 12:51:22 +0300

    added JsonReader and used in HomeTest

--- src/main/java/utils/JsonReader.java
+++ src/main/java/utils/JsonReader.java
@@ -2,6 +2,12 @@ package utils;
 //Data generation has 2 types (Dynamic - Static (Hardcoded))
 
 
+import com.jayway.jsonpath.JsonPath;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileReader;
+
 /*
 1. static (best of the best)
     1.1 Database snapshot (restore)
@@ -14,6 +20,26 @@ package utils;
 */
 // excel (not recommended) -> complex / no version control - csv (not recommended) -> complex / no version control - json - properties -> sys.get("email") -> take a key and return a value
 public class JsonReader {
+    String jsonReader;
+    String jsonFileName;
+    private final String test_data_path = "src/test/java/test-data";
+    public JsonReader(String jsonFileName){
+        this.jsonFileName = jsonFileName;
+        try{
+            JSONObject data = (JSONObject) new JSONParser().parse(new FileReader(test_data_path +"/"+ jsonFileName+".json"));
+            jsonReader = data.toJSONString();
+        } catch (Exception e) {
+            System.out.println("Error reading JSON file: " + e.getMessage());
+        }
+    }
 
+    public String getJsonReader(String jsonPath) {
+        try{
+            return JsonPath.read(jsonReader, jsonPath).toString();
+        }catch (Exception e){
+            System.out.println("Error getting JSON data: " + e.getMessage());
+            return "";
+        }
+    }
 
 }
--- src/test/java/tests/HomeTest.java
+++ src/test/java/tests/HomeTest.java
@@ -4,18 +4,21 @@ import drivers.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.json.Json;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.LoginPage;
+import utils.JsonReader;
 
 public class HomeTest {
     WebDriver driver;
+    JsonReader jsonReader;
 
     @Test
     public void addToCartTC(){
         new LoginPage(driver) //anonymous object
-                .login("standard_user", "secret_sauce")
+                .login(jsonReader.getJsonReader("username"), jsonReader.getJsonReader("password"))
                 .isLoggedIn("https://www.saucedemo.com/inventory.html")
                 .addToCart()
                 .validateCartCount();
@@ -25,6 +28,7 @@ public class HomeTest {
     //Configuration
     @BeforeMethod
     public void setUp(){
+        jsonReader = new JsonReader("home-data");
         driver = WebDriverFactory.initDriver();
         driver.get("https://www.saucedemo.com");
 
