be395086433a9df6d79f09fd6826936ab0f03ff5
YousefFawzy2222 <117592164+YousefFawzy2222@users.noreply.github.com>
2026-05-25 19:46:33 +0300

    Adding allure setup and some reproting informetive lines

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,9 @@
 package tests;
 
 import drivers.WebDriverFactory;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -14,7 +17,18 @@ public class LoginTest {
 
     //Tests
     @Test
+    //@DisplayName("Valid Login Test") //Change the name with TestNG itself
+    @Description("Verify that the user is redirected to home page after providing correct credentials") //Change the description of the testcase in the Allure report
+    @Tag("validLogin")
+    @Owner("Yousef")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link("www.confluence.jira.com/login") // any useful link related to the test case like a ticket link or a documentation link
+    @TmsLink("TC-001") // link to the test case in the test management system (Zephyr for example)
+    @Issue("BUG-01") //Link the test case with an issue
     public void validLoginTest(){
+        Allure.getLifecycle().updateTestCase(results ->
+                results.setName("Valid Login") //Change the name of the testcase in the Allure report
+        );
         new LoginPage(driver) //anonymous object
                 .login(PropertyReader.getProperty("validUsername"), PropertyReader.getProperty("validPassword"))
                 .isLoggedIn("https://www.saucedemo.com/inventory.html");
